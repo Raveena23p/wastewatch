492889738e9fd79ead9cc8ffe626760ca66daaba
Raveena23p <iraveena.p@gmail.com>
2025-03-17 07:16:14 -0600

    Model Update

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -20,9 +20,9 @@ export async function POST(request: Request) {
         const message = body?.message as string;
         const data = body?.data;
         const model = genAI.getGenerativeModel({
-            model: "gemini-1.5-flash",
+            model: "gemini-2.0-flash-lite",
             systemInstruction: `You are an friendly AI assistant in a Waste management app that contains data such as fillLevel as level in percentage filled, temperature in celsius, humidity in percentage, gas production in ppm, battery charge of a bin in percentage and the timestamp of the data inserted by the sensors. You will be given the data of that 
-            bin in a stringified JSON format within this delimitter ''', Based on those data you should analyse all of it and provide valuable insights for the users queries. Whenever the value of Level is 0 it means that the bin is emptied. The bin height is 200cms. The data is as following
+            bin in a stringified JSON format within this delimitter ''', Based on those data you should analyse all of it and provide valuable insights for the users queries. Whenever the value of Level is 0 it means that the bin is emptied. The bin height is 200cms.Always give the answer in 200 words. Give importance to the numerical data. Whenever the fill level is 0,it means the bin has been emptied. Apart from waste management or details regarding bin or bin management, do not answer any questions.When the question is asked about scheduling the next collection trip, answer with the approximate date analysing the past trend on how often the bin was filled.While the user is asking for tips to manage odour, give them reply.The data is as following
 
             '''${JSON.stringify(data)}'''`,
         });
